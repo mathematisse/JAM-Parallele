4ad949f70d5458156f039898539e0e3fddbb1a98
Alexandre Frede <alexandre.frede.pro@gmail.com>
2023-03-25 14:15:24 +0100

    Add just the ScriptableObjects

---
+++ Assets/Scripts/ScriptableBuilding.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BuildingType
+{
+    Tower,
+    Stock,
+    Casern
+}
+
+[CreateAssetMenu(fileName = "New Building", menuName = "Building")]
+public class ScriptableBuilding : ScriptableObject
+{
+    public Sprite Sprites;
+    public int WoodCost;
+    public int StoneCost;
+    public int MushroomCost;
+    public int SoulCost;
+    public BuildingType BuildingType;
+
+    public float AttackPower;
+    public float AttackSpeed;
+    public float AttackRange;
+    public float Hp;
+
+    public int WoodStorage;
+    public int StoneStorage;
+}
