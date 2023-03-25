1a6088e8f11a68c54d4e385af7cb0322d7745644
Alexandre Frede <alexandre.frede.pro@gmail.com>
2023-03-25 15:19:11 +0100

    Add build sprite

--- Assets/Scripts/BuildingScript.cs
+++ Assets/Scripts/BuildingScript.cs
@@ -6,12 +6,20 @@ public class BuildingScript : MonoBehaviour
 {
 
     public ScriptableBuilding Building;
+    public GameObject mirroredObject;
+
+    public float yOffset;
 
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<SpriteRenderer>().sprite = Building.Sprite;
-        Debug.Log("Sprite name: " + Building.Sprite.name);
+
+        mirroredObject = new GameObject(this.GetType().Name + " mirrored");
+        mirroredObject.transform.position = new Vector3(transform.position.x, transform.position.y - yOffset, transform.position.z);
+        SpriteRenderer mirroredSpriteRenderer = mirroredObject.AddComponent<SpriteRenderer>();
+        mirroredSpriteRenderer.sprite = Building.Sprite;
+        mirroredSpriteRenderer.flipY = true;
     }
 
     // Update is called once per frame
