2284e90de43e02c6de6dc78a48d6a0127d5d52f5
mathematisse <lucien.pineau@epitech.eu>
2023-03-25 23:25:49 +0100

    Link Ressources, Buildings & UI

--- Assets/Scripts/BuildingScript.cs
+++ Assets/Scripts/BuildingScript.cs
@@ -4,22 +4,49 @@ using UnityEngine;
 
 public class BuildingScript : MonoBehaviour
 {
-
+    public bool Built;
     public ScriptableBuilding Building;
     public GameObject mirroredObject;
 
     public float yOffset;
 
+    private CursorManager _cursorManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<SpriteRenderer>().sprite = Building.Sprite;
+        _cursorManager = FindObjectOfType<CursorManager>();
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool IsHovered(Vector2 point)
+    {
+        var overlapPoint = Physics2D.OverlapPoint(point);
+        if (overlapPoint != null && overlapPoint.gameObject == gameObject)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void Build()
+    {
+        if (_cursorManager.selected is not { type: SelectableType.Building })
+            return;
+        Building = _cursorManager.selected as ScriptableBuilding;
+
+        GetComponent<SpriteRenderer>().sprite = Building.Sprite;
         mirroredObject = new GameObject(this.GetType().Name + " mirrored");
         mirroredObject.transform.position = new Vector3(transform.position.x, transform.position.y - yOffset, transform.position.z);
         SpriteRenderer mirroredSpriteRenderer = mirroredObject.AddComponent<SpriteRenderer>();
         mirroredSpriteRenderer.sprite = Building.Sprite;
         mirroredSpriteRenderer.flipY = true;
+        Built = true;
     }
 
     void Upgrade()
@@ -29,11 +56,5 @@ public class BuildingScript : MonoBehaviour
 
         GetComponent<SpriteRenderer>().sprite = Building.Sprite;
         mirroredObject.GetComponent<SpriteRenderer>().sprite = Building.Sprite;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
